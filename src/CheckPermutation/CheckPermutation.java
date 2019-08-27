7223acbd289b8a5705ba6f1f54c8ea04731feb1a
blackbeltdiaries <github email address>
2019-08-27 17:16:41 -0600

    Problem 2: Check Permutation

--- src/CheckPermutation/CheckPermutation.java
+++ src/CheckPermutation/CheckPermutation.java
@@ -1,7 +1,34 @@
+//Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+
 package CheckPermutation;
 
 public class CheckPermutation {
-	public static void main(String[] str) {
-		System.out.print("hello");
+
+	// Do the two strings have the same characters, but in different order?
+
+	public static String sort(String s) {
+		char[] content = s.toCharArray();
+		java.util.Arrays.parallelSort(content);
+
+		String strContent = new String(content);
+		System.out.print(strContent);
+		return strContent;
+	}
+
+	public static boolean permutation(String s, String t) {
+		if (s.length() != t.length()) {
+			System.out.print("false");
+			return false;
+		}
+		return sort(s).equals(sort(t));
+	}
+
+	// Run program
+
+	public static void main(String[] args) {
+		String strOne = "yes";
+		String strTwo = "sey";
+
+		permutation(strOne, strTwo);
 	}
 }
